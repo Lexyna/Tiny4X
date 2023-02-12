a474fd6e6e425bab8eec0c0c1a70be26c8c425de
Lexyna <lexieverena@outlook.de>
2023-02-12 12:44:17 +0100

    Initial commit

---
+++ Entry.cs
@@ -0,0 +1,8 @@
+﻿
+using Terminal.Gui;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Application.Run<Tiny4X.UI.Terminal>();
+
+Application.Shutdown();
---
+++ src/UI/Terminal.cs
@@ -0,0 +1,15 @@
+using System;
+using Terminal.Gui;
+
+namespace Tiny4X.UI
+{
+
+    public class Terminal : Window
+    {
+
+        public Terminal()
+        {
+            Title = "Tiny4X";
+        }
+    }
+}
