9ad9c628bca62fed2a975559897d2a636efe4fa5
Lexyna <lexieverena@outlook.de>
2023-02-16 18:31:35 +0100

    Added new Field type 'Forest'

--- src/GameUtils/Map/TerrainGenerator.cs
+++ src/GameUtils/Map/TerrainGenerator.cs
@@ -8,10 +8,21 @@ namespace Tiny4X.GameUtils.Map
         {
             Field[,] map = new Field[height, length];
 
+            Random rnd = new Random();
+
             for (int y = 0; y < height; y++)
                 for (int x = 0; x < length; x++)
                 {
-                    map[y, x] = new GrassLand(x, y);
+                    int r = rnd.Next(0, 100);
+
+                    Field field;
+
+                    if (r > 50)
+                        field = new GrassLand(x, y);
+                    else
+                        field = new Forest(x, y);
+
+                    map[y, x] = field;
                 }
 
             return map;
